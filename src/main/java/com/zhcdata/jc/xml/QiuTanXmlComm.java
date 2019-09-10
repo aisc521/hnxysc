927ce4db9ae45b17e19693d51e93df019291b2ad
gaoyang <gaoyang@zhcw.com>
2019-09-10 20:20:10 +0800

    init

--- src/main/java/com/zhcdata/jc/xml/QiuTanXmlComm.java
+++ src/main/java/com/zhcdata/jc/xml/QiuTanXmlComm.java
@@ -36,6 +36,22 @@ public class QiuTanXmlComm<T>  {
 
     return xStream.fromXML(xml);
   }
+  public List<T> handleMothodList(String url, Class ...clas){
+    String xml = "";
+    try {
+      xml = HttpUtils.httpPost(url,"UTF-8");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    XStream xStream = new XStream(new DomDriver());
+    XStream.setupDefaultSecurity(xStream);
+    //xStream.allowTypes(new Class[]{List.class,clas});
+    xStream.allowTypes(clas);
+    xStream.processAnnotations(clas);
+    List<T> list = (List<T>) xStream.fromXML(xml);
+
+    return list;
+  }
   public static void main(String argsp[]){
     //String xml = HttpUtils.httpPost("http://interface.win007.com/zq/today.aspx","UTF-8");
    /* List<ToDayMatchRsp> list  = (List<ToDayMatchRsp>) new QiuTanXmlComm().handleMothod("http://interface.win007.com/zq/today.aspx",List.class,ToDayMatchRsp.class);
