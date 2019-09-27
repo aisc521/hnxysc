bd8538659ea900c460a293bdb471bc09cc37af0f
xuan <xuan313@outlook.com>
2019-09-27 17:57:26 +0800

    赔率解析完成提交

---
+++ src/main/java/com/zhcdata/jc/dto/SimplifyOdds.java
@@ -0,0 +1,44 @@
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　create by xuan on 2019/9/26
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ */
+package com.zhcdata.jc.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SimplifyOdds {
+
+    private Integer oddsId;
+    private String win;
+    private String draw;
+    private String lose;
+
+    private String win_now;
+    private String draw_now;
+    private String lose_now;
+
+}
