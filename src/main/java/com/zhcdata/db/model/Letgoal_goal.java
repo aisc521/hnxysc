9d923261a689f18585c7c6d6ae1d927703642937
xuan <xuan313@outlook.com>
2019-10-11 10:13:35 +0800

    新增亚盘变盘次数表操作

---
+++ src/main/java/com/zhcdata/db/model/Letgoal_goal.java
@@ -0,0 +1,45 @@
+package com.zhcdata.db.model;
+
+import java.util.Date;
+
+public class Letgoal_goal {
+    private Long oddsid;
+
+    private Integer goalCount;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public Long getOddsid() {
+        return oddsid;
+    }
+
+    public void setOddsid(Long oddsid) {
+        this.oddsid = oddsid;
+    }
+
+    public Integer getGoalCount() {
+        return goalCount;
+    }
+
+    public void setGoalCount(Integer goalCount) {
+        this.goalCount = goalCount;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
