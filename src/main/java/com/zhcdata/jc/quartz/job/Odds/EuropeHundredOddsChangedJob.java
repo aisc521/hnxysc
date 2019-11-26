10eab2427f77fd675fca8b3ed1e40c8e21e1daf8
xuan <xuan313@outlook.com>
2019-11-26 17:24:40 +0800

    此条子表数据不存在于主表时，新增主表数据和子表数据

--- src/main/java/com/zhcdata/jc/quartz/job/Odds/EuropeHundredOddsChangedJob.java
+++ src/main/java/com/zhcdata/jc/quartz/job/Odds/EuropeHundredOddsChangedJob.java
@@ -54,55 +54,65 @@ public class EuropeHundredOddsChangedJob implements Job {
         String str = null;
         try {
             str = HttpUtils.httpGet("http://interface.win007.com/zq/1x2.aspx?min=4", null);
-            if (str.contains("访问频率超出限制")){
+            if (str.contains("访问频率超出限制")) {
                 log.error("百欧赔率变化解析完成,访问频率超出限制");
                 return;
             }
 
         } catch (Exception e) {
-            LOGGER.error("百欧赔率变化解析获取变化失败",e);
+            LOGGER.error("百欧赔率变化解析获取变化失败", e);
             return;
         }
         EuropeHundredOddsRsp obj = com.alibaba.fastjson.JSONObject.parseObject(XML.toJSONObject(str).toString(), EuropeHundredOddsRsp.class);
         List<H> items = obj.getC().getH();//每场比赛
         for (int i = 0; i < items.size(); i++) {
             try {
-                if (sdf_X.parse(items.get(i).getTime()).getTime()<System.currentTimeMillis())
+                if (sdf_X.parse(items.get(i).getTime()).getTime() < System.currentTimeMillis())
                     continue;
             } catch (ParseException e) {
-                LOGGER.error("百欧赔率表接口,比赛时间格式化失败，跳过比赛"+items.get(i).getId());
+                LOGGER.error("百欧赔率表接口,比赛时间格式化失败，跳过比赛" + items.get(i).getId());
                 continue;
             }
             List<String> o = items.get(i).getOdds().getO();//每家公司的赔率
-            if (BeanUtils.parseTime(items.get(i).getTime()).getTime()<System.currentTimeMillis())
+            if (BeanUtils.parseTime(items.get(i).getTime()).getTime() < System.currentTimeMillis())
                 continue;
-            for (int j = 0; j < o.size(); j++) {
-                String[] mo = o.get(j).split(",");//当前比赛 当前公司
+            for (String s : o) {
+                String[] mo = s.split(",");//当前比赛 当前公司
                 //博彩公司ID,博彩公司英文名,初盘主胜,初盘平局,初盘客胜,主胜,平局,客胜,变化时间,博彩公司简体名
-                //根据比赛 公司 查询这个赔率 如果没有跳过
-                EuropeOdds dbl = europeOddsMapper.selectByMidAndCpyAnd(items.get(i).getId(),mo[0]);
-                if (dbl==null)
-                    nexted++;
-                else {
+                //根据比赛 公司 查询这个赔率 如果没有 新增主表 (之前是跳过)
+                EuropeOdds dbl = europeOddsMapper.selectByMidAndCpyAnd(items.get(i).getId(), mo[0]);
+                if (dbl == null) {
+                    try {
+                        nexted++;
+                        EuropeOdds xml = new BeanUtils().parseEuropeOdds(items.get(i).getId(), mo);
+                        europeOddsMapper.insertSelective(xml);
+                        dbl = europeOddsMapper.selectByMidAndCpyAnd(items.get(i).getId(), mo[0]);
+                        EuropeOddsDetail detail = BeanUtils.parseEuropeOddsDetail(mo);
+                        detail.setOddsid(dbl.getOddsid());
+                        detailMapper.insertSelective(detail);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
                     //这局oddsId查询最新的
                     EuropeOddsDetail db = detailMapper.selectByOddsNewest(dbl.getOddsid());
                     EuropeOddsDetail xml = BeanUtils.parseEuropeOddsDetail(mo);
-                    if (db==null){
-                            ////第一次添加
-                            //xml.setOddsid(dbl.getOddsid());
-                            //if (detailMapper.insertSelective(xml)>0) {
-                            //    insert++;
-                            //    log.error("百欧详情表新增新增"+xml.toString());
-                            //}
-                            ////变更主表
-                            //dbl.setRealstandoff(xml.getStandoff());
-                            //dbl.setRealhomewin(xml.getHomewin());
-                            //dbl.setRealguestwin(xml.getGuestwin());
-                            //dbl.setStandoffR(xml.getStandoff());
-                            //dbl.setHomewinR(xml.getHomewin());
-                            //dbl.setGuestwinR(xml.getGuestwin());
-                            //europeOddsMapper.updateByPrimaryKeySelective(dbl);
-                    }else {
+                    if (db == null) {
+                        //第一次添加
+                        //xml.setOddsid(dbl.getOddsid());
+                        //if (detailMapper.insertSelective(xml)>0) {
+                        //    insert++;
+                        //    log.error("百欧详情表新增新增"+xml.toString());
+                        //}
+                        ////变更主表
+                        //dbl.setRealstandoff(xml.getStandoff());
+                        //dbl.setRealhomewin(xml.getHomewin());
+                        //dbl.setRealguestwin(xml.getGuestwin());
+                        //dbl.setStandoffR(xml.getStandoff());
+                        //dbl.setHomewinR(xml.getHomewin());
+                        //dbl.setGuestwinR(xml.getGuestwin());
+                        //europeOddsMapper.updateByPrimaryKeySelective(dbl);
+                    } else {
                         if (db.oddsEquals(xml))
                             jumped++;//相同时间 相同赔率 跳过
                         else {
@@ -112,9 +122,9 @@ public class EuropeHundredOddsChangedJob implements Job {
                             db.setGuestwin(xml.getGuestwin());
                             db.setStandoff(xml.getStandoff());
                             db.setHomewin(xml.getHomewin());
-                            if (detailMapper.insertSelective(db)>0) {
+                            if (detailMapper.insertSelective(db) > 0) {
                                 update++;
-                                log.error("百欧详情表新增更新"+db.toString());
+                                log.error("百欧详情表新增更新" + db.toString());
                             }
                             //变更主表
                             dbl.setRealstandoff(xml.getStandoff());
@@ -129,6 +139,6 @@ public class EuropeHundredOddsChangedJob implements Job {
                 }
             }
         }
-        LOGGER.info("百欧赔率变化解析完成,新增:"+insert+",跳过:"+jumped+",更新:"+update+",next:"+nexted+"耗时:"+(System.currentTimeMillis()-sat));
+        LOGGER.info("百欧赔率变化解析完成,新增:" + insert + ",跳过:" + jumped + ",更新:" + update + ",next:" + nexted + "耗时:" + (System.currentTimeMillis() - sat));
     }
 }
